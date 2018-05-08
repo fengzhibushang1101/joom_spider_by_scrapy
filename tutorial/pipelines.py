d86d255261bbbeba08569f3cc72482f9316a41da
yitian <fengzhibushang@163.com>
2018-05-08 18:52:33 +0800

    remove yield

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -42,9 +42,13 @@ class CategoryPipeline(object):
         self.session_count = 0
 
     def process_item(self, item, spider):
+        # return item
         if isinstance(item, CategoryItem):
             sql_str = text(
                 "insert into category (tag, category.name, p_id, is_leaf, category.level, site_id, status) values (:tag, :name, :p_id, :is_leaf, :level, :site_id, :status) on duplicate key update status=:status")
+            self.session.execute(sql_str, dict(item))
+            self.session.commit()
+            return item
         elif isinstance(item, ShopItem):
             sql_str = text('insert into joom_shop (joom_shop.name,shop_no,logo,rate,save_count,create_time,update_time,is_verify,pro_count,reviews_count,r_count_30,r_count_7,r_count_7_14,growth_rate,cate_id) values (:name,:shop_no,:logo,:rate,:save_count,:create_time,:update_time,:is_verify,0,0,0,0,0,0,"") on duplicate key update rate=:rate, save_count=:save_count, create_time=:create_time, update_time=:update_time, is_verify=:is_verify;')
         elif isinstance(item, ProductItem):
@@ -55,8 +59,8 @@ class CategoryPipeline(object):
             sql_str = text('insert into joom_user (user_no, full_name, images) values (:user_no, :full_name, :images) on duplicate key update full_name=:full_name, images = :images;')
         else:
             sql_str = text('insert into joom_review (review_no,create_time,update_time,pro_no,variation_id,user_no,joom_review.language,origin_text,new_text,order_id,is_anonymous,colors,star,shop_no,photos) VALUES (:review_no,:create_time,:update_time,:pro_no,:variation_id,:user_no,:language,:origin_text,:new_text,:order_id,:is_anonymous,:colors,:star,:shop_no,:photos) on duplicate key update star=:star;')
-        self.session_count += 1
         self.session.execute(sql_str, dict(item))
+        self.session_count += 1
         if self.session_count > 10000:
             self.session_count = 0
             self.session.commit()
